f3c7607e342ca4e2267af1008e1aed10fa08d87c
gabyformula1 <gabriel.henaux@edu.ece.fr>
2023-09-27 09:37:55 +0200

    feat: db initialization

--- Labs/Lab_3/index.js
+++ Labs/Lab_3/index.js
@@ -1,6 +1,27 @@
 const http = require('http')
 const handles = require('./handles')
+const express = require('express')
+const router = express.Router()
 
+router.use((req, res, next) => {
+    console.log('Time: ', Date.now())
+    next()
+  })
+
+router.get('/', (req, res) => {
+    res.send('Birds home page')
+})
+
+router.get('/about', (req, res) => {
+    res.send('About birds')
+})
+
+module.exports = router
+
+const birds = require('./birds')
+
+
+app.use('/birds', birds)
 http
 .createServer(handles.serverHandle)
 .listen(8080)
