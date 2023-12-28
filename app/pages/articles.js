500f43e27d41781c27f298495fd48026b6d03a27
MisterTanguyBeen <tanguy.velay@gmail.com>
2023-12-28 17:15:20 +0100

    feat: style index.js

--- app/pages/articles.js
+++ app/pages/articles.js
@@ -32,11 +32,13 @@ export default function Articles({ articles, page, totalPages }) {
         
       <h1 className='wt-title'>Ferrari's News Articles</h1>
       
-      <div className="flex items-center justify-center mb-5">
-        <p className="italic font-bold my-5">Create your article just here :</p>
-        <button onClick={handleNewArticleClick} className="create-article-button">
-              New Article
-        </button>
+      <div className="w-1/2 mx-auto">
+        <div className="flex items-center justify-center mb-5 bg-gray-100 rounded-lg">
+          <p className="italic font-bold my-5">Write you own article just here :</p>
+          <button onClick={handleNewArticleClick} className="create-article-button">
+                New Article
+          </button>
+        </div>
       </div>
       <p className="italic font-bold my-5 flex items-center justify-center">Discover our news articles about Ferrari</p>
       <ul className="">
